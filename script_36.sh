5f9fdbeebd752c5ac85ed11c9184fde7a41aa5b3
NHMQ25100 <nhmqnoeh@login03.css.lan>
2017-11-22 16:40:16 +0100

    scripts to compute 30000 matrices

---
+++ script_36.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -login
+#PBS -N mul_36
+#PBS -l nodes=10:ppn=4
+#PBS -l walltime=10:00:00
+#PBS -l mem=42gb
+#PBS -W x=PARTITION:lena 
+
+# show which computer the job ran on
+echo "Job ran on:" $(hostname)
+# change to working directory
+cd /home/nhmqnoeh
+
+module load intel
+# run the simulation
+mpirun -n 10 ./mpi_main36
