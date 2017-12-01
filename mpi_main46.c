a7e12376b23603c0e45fbd361cee9beaacfb7806
NHMQ25100 <nhmqnoeh@login03.css.lan>
2017-12-01 16:05:47 +0100

    4.6

---
+++ mpi_main46.c
@@ -0,0 +1,40 @@
+#include "functions.h"
+#include <mpi.h>
+#include <math.h>
+#include <stdio.h>
+
+int main(int argc, char* argv){
+        int rank;
+        int worldSize;
+        int N = 1000;
+
+        MPI_Init(NULL, NULL);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+        int chunkPerLine = sqrt(worldSize);
+        if(chunkPerLine * chunkPerLine != worldSize){
+                if(rank==0) printf("Please only use a square number of Processors\n");
+                return 1;
+        }
+
+        int* mat = malloc(sizeof(int)*N*N);
+
+	
+        char aPath[50];
+        snprintf(aPath, sizeof(aPath), "/bigwork/nhmqnoeh/A_%ix%i.bin", N, N);
+        if(rank==0){
+		read_matrix_bin(mat, aPath, N);
+        }
+        MPI_Bcast(mat, N*N, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if(rank==0)start_time();
+	snprintf(aPath, sizeof(aPath), "/bigwork/nhmqnoeh/A_%ix%i_striped.bin", N, N);
+
+	write_matrix_mpi_fw_stripe(mat, aPath, N);
+	MPI_Barrier(MPI_COMM_WORLD);
+	if(rank==0){
+		printf("writing took %llu ms\n", stop_time());
+	}
+	return 0;
+}
--- write_matrix.c
+++ write_matrix.c
@@ -85,6 +85,46 @@ int write_matrix_mpi_fw(int* data, char* filename, int N) {
         return 0;
 }
 
+int write_matrix_mpi_fw_stripe(int* data, char* filename, int N) {
+        MPI_File file;
+        int rank;
+        int worldSize;
+
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+
+        int chunkPerLine = sqrt(worldSize);
+        int blockWidth = N/chunkPerLine;
+        int startPosX = (rank%chunkPerLine)*blockWidth;
+        int startPosY = (int)(rank/chunkPerLine) * blockWidth * N;
+
+        MPI_Datatype dataBlock;
+        MPI_Type_contiguous(blockWidth, MPI_INT, &dataBlock);
+        MPI_Type_commit(&dataBlock);
+
+	int stripe_count = worldSize;
+	if(stripe_count > 20) stripe_count = 20;
+	int stripe_size = 10*N*N / stripe_count;
+	
+	char temp[100];
+	sprintf(temp, "%d", stripe_count);
+	
+	MPI_Info info;
+	MPI_Info_create(&info);
+	MPI_Info_set(info, "striping_factor", temp);
+	MPI_Info_set(info, "cb_nodes", temp);
+	sprintf(temp, "%d", stripe_size);
+	MPI_Info_set(info, "striping unit", temp);
+
+        MPI_File_open(MPI_COMM_WORLD, filename, MPI_MODE_CREATE | MPI_MODE_WRONLY, info, &file);
+        for(int i = 0; i < blockWidth; i++){
+                int startPos = startPosX+startPosY+i*N;
+                MPI_File_set_view(file, sizeof(int) * startPos, MPI_INT, dataBlock, "native", info);
+                MPI_File_write(file, &data[startPos], blockWidth, MPI_INT, MPI_STATUS_IGNORE);
+        }
+        MPI_File_close(&file);
+        return 0;
+}
 
 
 void print_matrix(int* matrix, int N){
